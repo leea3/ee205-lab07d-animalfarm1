de3d484835670c5d5fe9838ae7082e77a8dc8541
Arthur Lee <leea3@hawaii.edu>
2022-02-13 12:15:33 -1000

    added couple printf statements to main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Lab 05d - Animal Farm 0 - EE 205 - Spr 2022
+///
+/// @file main.c
+/// @version 1.0
+///
+///
+/// @author Arthur Lee <leea3@hawaii.edu>
+/// @date   21 Feb 2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   printf("Starting Animal Farm 0\n");
+
+
+
+   printf("Done with Animal Farm 0\n");
+}
