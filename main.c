b26a59c511a4409e335f4bf6403d3b270babecd8
Arthur Lee <leea3@hawaii.edu>
2022-02-13 17:07:43 -1000

    added some cat information to main.c, added debug lines

--- main.c
+++ main.c
@@ -12,11 +12,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "addCats.h"
+#include "catDatabase.h"
+
+#define DEBUG
 
 int main(){
    printf("Starting Animal Farm 0\n");
+   
+   initializeDatabase( );
 
+   addCat( "Loki",  MALE,           PERSIAN,    true,  8.5 ) ;
+   addCat( "Milo",  MALE,           MANX,       true,  7.0 ) ;
+   addCat( "Bella", FEMALE,         MAINE_COON, true,  18.2 ) ;
+   addCat( "Kali",  FEMALE,         SHORTHAIR,  false, 9.2 ) ;
+   addCat( "Trin",  FEMALE,         MANX,       true,  12.2 ) ;
+   addCat( "Chili", UNKNOWN_GENDER, SHORTHAIR,  false, 19.0 ) ;
+   
+#ifdef DEBUG
 
+   for( int i = 0 ; i < numberOfCats + 2; i++ ) {
+   printf("cat index = [%d] name=[%s] gender=[%d] breed=[%d] isFixed=[%d] weight=[%f] \n" , i , catName[i], catGender[i] , catBreed[i] , catFixed[i] , catWeight[i]);
+   }
+#endif
 
    printf("Done with Animal Farm 0\n");
 }
