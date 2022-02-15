b62cb10c5198ce78e95e86a4a05dfda537f2d964
Arthur Lee <leea3@hawaii.edu>
2022-02-15 10:28:15 -1000

    added deleteCats.c/.h and stubbed out functions

---
+++ deleteCats.c
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Lab 05d - Animal Farm 0 - EE 205 - Spr 2022
+///
+/// @file deleteCats.c
+/// @version 1.0
+///
+///
+/// @author Arthur Lee <leea3@hawaii.edu>
+/// @date   21 Feb 2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include "catDatabase.h"
+#include "deleteCats.h"
+
+//void deleteAllCats(
---
+++ deleteCats.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  Lab 05d - Animal Farm 0 - EE 205 - Spr 2022
+///
+/// @file deleteCats.h
+/// @version 1.0
+///
+///
+/// @author Arthur Lee <leea3@hawaii.edu>
+/// @date   21 Feb 2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+extern void deleteAllCats( );
