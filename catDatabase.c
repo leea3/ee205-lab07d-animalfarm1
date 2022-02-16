96e7c4ffe64aad543427b3d5c1c2cfec7e813f93
Arthur Lee <leea3@hawaii.edu>
2022-02-15 15:35:48 -1000

    improved documentation for catDatabase.c/.h and removed debug lines

--- catDatabase.c
+++ catDatabase.c
@@ -5,6 +5,8 @@
 /// @file catDatabase.c
 /// @version 1.0
 ///
+/// @brief Contains global variables that contains cat information
+/// as well as a function to initialize the database
 ///
 /// @author Arthur Lee <leea3@hawaii.edu>
 /// @date   21 Feb 2022
@@ -15,6 +17,7 @@
 #include <string.h>
 #include "catDatabase.h"
 
+//stored number of cats in database as global variables
 int numberOfCats;
 
 //stored arrays as global variables
@@ -24,7 +27,6 @@ enum Breed catBreed[MAX_CATS];
 bool catFixed[MAX_CATS];
 double catWeight[MAX_CATS];
 
-//initializes database to default
 void initializeDatabase( ) {
    numberOfCats= 0;
 
@@ -35,9 +37,6 @@ void initializeDatabase( ) {
       catFixed[i] = false;
       catWeight[i] = 0.0;
 
-#ifdef debug
-      printf("cat name: [%s]\n", catName[MAX_CATS]);
-#endif
    }
 }
 
--- catDatabase.h
+++ catDatabase.h
@@ -5,6 +5,8 @@
 /// @file catDatabase.h
 /// @version 1.0
 ///
+/// @brief Contains global variables that contains cat information
+/// as well as a function to initialize the database
 ///
 /// @author Arthur Lee <leea3@hawaii.edu>
 /// @date   21 Feb 2022
@@ -19,12 +21,18 @@
 enum Gender {  UNKNOWN_GENDER = 0 , MALE = 1 , FEMALE = 2 };
 enum Breed { UNKNOWN_BREED = 0 , MAINE_COON = 1 , MANX = 2 , SHORTHAIR = 3 , PERSIAN = 4 , SPHYNX = 5 };
 
+//Note: arrays held as global variables
 extern char catName[][CATNAME_CHARLIMIT];
 extern enum Gender catGender[];
 extern enum Breed catBreed[];
 extern bool catFixed[];
 extern double catWeight[];
 
+//Note: Current number of cats in database held as a global variable
 extern int numberOfCats;
 
+/* Zeroes out the database based on MAX_CATS
+ *
+ * zeros out the five arrays and number of cats in database
+ */
 extern void initializeDatabase( );
