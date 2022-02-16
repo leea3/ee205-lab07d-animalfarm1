8ba90bb16402ce4dcf2a3f4f19387a5d382c30af
Arthur Lee <leea3@hawaii.edu>
2022-02-15 14:49:04 -1000

    Added lines that tests functionality of other .c files

--- main.c
+++ main.c
@@ -16,27 +16,51 @@
 #include <stdbool.h>
 #include "addCats.h"
 #include "catDatabase.h"
+#include "reportCats.h"
+#include "updateCats.h"
+#include "deleteCats.h"
 
-#define DEBUG
+//#define DEBUG
 
 int main(){
    printf("Starting Animal Farm 0\n");
    
    initializeDatabase( );
 
-   addCat( "Loki",  MALE,           PERSIAN,    true,  8.5 ) ;
-   addCat( "Milo",  MALE,           MANX,       true,  7.0 ) ;
-   addCat( "Bella", FEMALE,         MAINE_COON, true,  18.2 ) ;
-   addCat( "Kali",  FEMALE,         SHORTHAIR,  false, 9.2 ) ;
-   addCat( "Trin",  FEMALE,         MANX,       true,  12.2 ) ;
-   addCat( "Chili", UNKNOWN_GENDER, SHORTHAIR,  false, 19.0 ) ;
-   
+   addCat( "Loki", MALE, PERSIAN, true, 8.5 ) ;
+   addCat( "Milo", MALE, MANX, true, 7.0 ) ;
+   addCat( "Bella", FEMALE, MAINE_COON, true, 18.2 ) ;
+   addCat( "Kali", FEMALE, SHORTHAIR, false, 9.2 ) ;
+   addCat( "Trin", FEMALE, MANX, true, 12.2 ) ;
+   addCat( "Chili", UNKNOWN_GENDER, SHORTHAIR, false, 19.0 ) ;
+
+   printAllCats();
+
+   int kali = findCat( "Kali" ) ;
+   updateCatName( kali, "Chili" ) ; // this should fail
+   printCat( kali );
+   updateCatName( kali , "Capulet" ) ;
+   updateCatWeight( kali, -5 );
+   updateCatWeight( kali, 9.9 ) ;
+   fixCat( kali ) ;
+   printCat( kali );
+
+   printAllCats();
+
+   deleteCat( kali );
+   printAllCats();
+
+   deleteAllCats();
+   printAllCats();   
+
 #ifdef DEBUG
 
-   for( int i = 0 ; i < numberOfCats + 2; i++ ) {
+   for( int i = 0 ; i < MAX_CATS ; i++ ) {
    printf("cat index = [%d] name=[%s] gender=[%d] breed=[%d] isFixed=[%d] weight=[%f] \n" , i , catName[i], catGender[i] , catBreed[i] , catFixed[i] , catWeight[i]);
    }
+
 #endif
 
    printf("Done with Animal Farm 0\n");
 }
+
