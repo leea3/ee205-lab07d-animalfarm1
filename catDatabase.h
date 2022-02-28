1d7a35104f848011e3dd26374188bac96751b9ea
leea3 <leea3@hawaii.edu>
2022-02-27 15:20:31 -1000

    Added color enum and added 3 new fields: collarColor1, collarColor2, and license

--- catDatabase.h
+++ catDatabase.h
@@ -20,11 +20,15 @@
 
 enum Gender {  UNKNOWN_GENDER = 0 , MALE = 1 , FEMALE = 2 };
 enum Breed { UNKNOWN_BREED = 0 , MAINE_COON = 1 , MANX = 2 , SHORTHAIR = 3 , PERSIAN = 4 , SPHYNX = 5 };
+enum Color { BLACK = 0 , WHITE = 1 , RED = 2 , BLUE = 3 , GREEN = 4 , PINK = 5 };
 
 struct cat_database{
     char name[CATNAME_CHARLIMIT];
     enum Gender gender;
     enum Breed breed;
+    enum Color collarColor1;
+    enum Color collarcolor2;
+    unsigned long long license;
     bool fixed;
     double weight;
 };
